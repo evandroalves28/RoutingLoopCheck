cf1f88b951fe9cae3748eba509314be226697a0b
Evandro Alves <41793892+evandroalves28@users.noreply.github.com>
2022-11-28 16:40:53 -0300

    Update routingloopcheck.sh

--- routingloopcheck.sh
+++ routingloopcheck.sh
@@ -4,31 +4,45 @@
  ############################################################
  # Verificação de loop de roteamento                        #
  # by Evandro Alves (for Intercol)                          #
- # Versão 1.2811722                                         #
+ # Versão 1.281172203                                       #
  # Documentacao completa em:                                #
- # https://github.com/evandroalves28/RoutingLoopCheck   #
+ # https://github.com/evandroalves28/RoutingLoopCheck       #
  ############################################################
 
 #
 #
 #Altere estas variáveis conforme o ambiente 
 #
+#Habilita o registro de logs
+SAVE_LOG=NO
+#LOG_PATH=/var/log/
 #Habilita o envio dos dados por email
-SEND_EMAIL=YES
-DST_MAIL=suporte@intercol.com.br
+SEND_EMAIL=NO
+DST_MAIL=root@localhost
 #
 #
 #
 #
 if [ -z ${1} ] ; then
         echo "Você precisa especificar o ASN a ser verificado"
-        echo "Usage example: ./netloopcheck.sh 65535"
+        echo "Usage example: ./routingloopcheck.sh 65535 #Irá testar os CIDR alocados para o AS65535"
+        echo "\n"
+        echo "Usage example: ./routingloopcheck.sh 65535 65534#Irá testar os CIDR alocados para os AS65535 e AS65534"
+        echo "\n"        
+        echo "Usage example: ./routingloopcheck.sh -c 65535 #Irá testar os CIDR alocados para os clientes do AS65535"
+        echo "Obs: compativel somente quando a route policy esta declarada no nic.br"
         exit 1
 fi
-declare -a ASNARRAY=("$@")
+
+if [ ${1} = "-c" ] ; then
+        declare -a ASNARRAY=(`whois -h whois.nic.br "$2" 2>&1 | grep -w 'as-in:' |  grep -oE 'accept AS([0-9]{1,6})' | grep -oE '([0-9]{1,6})'`)
+else
+        declare -a ASNARRAY=("$@")
+fi
+
 for ASN in "${ASNARRAY[@]}"
 do
-INETNUM=`whois -h whois.nic.br "$ASN" 2>&1 | grep -w 'inetnum:' | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\/([0-9]){1,2}\b'`
+ INETNUM=`whois -h whois.nic.br "$ASN" 2>&1 | grep -w 'inetnum:' | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\/([0-9]){1,2}\b'`
 #
         declare -a CIDRARRAY=($INETNUM)
         for CIDR in "${CIDRARRAY[@]}"
